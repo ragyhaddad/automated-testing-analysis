9d4154a82a37c9638f2b4c10eb46494c45a1b75b
rhaddad7 <rhaddad7@gatech.edu>
2018-08-06 13:00:27 -0400

    final model/ multipleRegression

--- multipleRegression.py
+++ multipleRegression.py
@@ -1,4 +1,4 @@
-#!/bin/bash/python
+#!/bin/bash/env python
 import sys
 import pandas as pd 
 import numpy as np
@@ -6,21 +6,33 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
+if(len(sys.argv) < 2):
+    print 'Usage: python multipleRegression.py <tests_input.csv>'
+    sys.exit(1)
+
+
 #Import data
 df = pd.read_csv(sys.argv[1], skiprows=[1])
 df = df.dropna()
+df = df.head(2000)
+
 df = df.drop(df.index[len(df)-1])
 
 #Model and predictions
-predictors = [3,4,5,6,7,8,9]
-dependant_variable = 0
+predictors = [1,3,4,5]
+dependant_variable = 9
 print df.iloc[:,predictors].columns
 X = df.iloc[:,predictors].values
 Y = df.iloc[:,dependant_variable].values
 X = sm.add_constant(X)
 results = sm.OLS(exog = X, endog = Y).fit()
 predict_y = results.predict(X)
+
+compare_df = pd.DataFrame({'Original':Y, 'Predicted':predict_y})
+print compare_df
 print results.summary()
+
+
 
 
 
